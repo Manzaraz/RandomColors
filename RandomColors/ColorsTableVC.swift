645bf451cb262269761191a87dcc800807b9cdb8
Christian Manzaraz <magnusmanz@gmail.com>
2024-08-10 21:27:54 -0300

    UINavigationController - App     Navigation Setup UITableView - Setup

---
+++ RandomColors/ColorsTableVC.swift
@@ -0,0 +1,34 @@
+//
+//  ColorsTableVC.swift
+//  RandomColors
+//
+//  Created by Christian Manzaraz on 10/08/2024.
+//
+
+import UIKit
+
+class ColorsTableVC: UIViewController {
+        
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
+extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource  {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    }
+}
